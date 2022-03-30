66cbfc16cafdd27ab7ed0ca8ef9db96822a5fc7a
ich333 <lapinich@gmail.com>
2022-03-30 13:03:37 +0200

    epam java training sum and mult

---
+++ src/summa.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class summa {
+    public static void main(String[] args) {
+
+        Scanner vvod = new Scanner(System.in);
+        try {
+
+
+            int firstNumber;
+            int secondNumber;
+
+            System.out.println("enter first integer number");
+            firstNumber = vvod.nextInt();
+            System.out.println("enter 2nd integer number");
+            secondNumber = vvod.nextInt();
+
+            System.out.println("Sum= " + (firstNumber + secondNumber));
+            System.out.println("Mutli= " + (firstNumber * secondNumber));
+        } catch (Exception e) {
+           System.out.println("Entered not integer value");
+
+            }
+        }
+
+    }
+
